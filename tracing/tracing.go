444e0eb0907f0543afb86ef695f38d3d2e91f415
Admin <admin@Bhupendra-2.local>
2025-06-09 23:48:35 +0530

    trace detailed added

--- tracing/tracing.go
+++ tracing/tracing.go
@@ -2,7 +2,10 @@ package tracing
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"net/http"
+	"time"
 
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/jaeger"
@@ -10,6 +13,8 @@ import (
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 	"go.opentelemetry.io/otel/trace"
+
+	"math/rand"
 )
 
 var Tracer trace.Tracer
@@ -36,4 +41,34 @@ func InitTracer(serviceName string) func() {
 			log.Printf("Error shutting down tracer provider: %v", err)
 		}
 	}
+}
+
+func generateID() string {
+	return fmt.Sprintf("%x", rand.Int63())
+}
+
+func LoggingMiddleware(serviceName string, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		traceID := r.Header.Get("X-Trace-ID")
+		if traceID == "" {
+			traceID = generateID()
+		}
+
+		parentSpanID := r.Header.Get("X-Span-ID")
+		spanID := generateID()
+
+		start := time.Now()
+		r.Header.Set("X-Trace-ID", traceID)
+		r.Header.Set("X-Parent-Span-ID", parentSpanID)
+		r.Header.Set("X-Span-ID", spanID)
+
+		log.Printf("[%s] --> Request | TraceID=%s | ParentSpanID=%s | SpanID=%s | Method=%s | URL=%s",
+			serviceName, traceID, parentSpanID, spanID, r.Method, r.URL.Path)
+
+		next.ServeHTTP(w, r)
+
+		duration := time.Since(start)
+		log.Printf("[%s] <-- Response | TraceID=%s | SpanID=%s | Duration=%s",
+			serviceName, traceID, spanID, duration)
+	})
 }
