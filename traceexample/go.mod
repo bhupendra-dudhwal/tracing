327db2f2f0ef07a405f8ce685b838d7085470b2f
Admin <admin@Bhupendra-2.local>
2025-06-09 23:33:08 +0530

    traceexample added

---
+++ traceexample/go.mod
@@ -0,0 +1,3 @@
+module github.com/bhupendra-dudhwal/traceexample
+
+go 1.21.0
---
+++ traceexample/service_a/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.Handle("/start", middleware.LoggingMiddleware("service1", http.HandlerFunc(handleStart)))
+	log.Println("[service1] ✅ Listening on :4001")
+	http.ListenAndServe(":4001", nil)
+}
+
+func handleStart(w http.ResponseWriter, r *http.Request) {
+	req, _ := http.NewRequest("GET", "http://service2:4002/work", nil)
+	req.Header.Set("X-Trace-ID", r.Header.Get("X-Trace-ID"))
+	req.Header.Set("X-Span-ID", r.Header.Get("X-Span-ID"))
+
+	client := &http.Client{Timeout: 3 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("[service1] ❌ Error calling service2: %v\n", err)
+		http.Error(w, "service2 failed", 500)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Fprintln(w, "Service1 completed call to Service2")
+}
